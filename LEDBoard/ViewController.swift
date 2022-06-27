072aa77e97bbc8c39194b61d877ccb7f8e2503b0
AbletonPilot <sonjm0428@gmail.com>
2022-06-27 09:18:38 +0900

    06.27.2022

--- LEDBoard/ViewController.swift
+++ LEDBoard/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, LEDBoardSettingDelegate {
+	@IBOutlet weak var contentsLabel: UILabel!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		self.contentsLabel.textColor = .yellow
 		// Do any additional setup after loading the view.
 	}
+	
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		if let settingViewController = segue.destination as? settingViewController {
+			settingViewController.delegate = self
+			settingViewController.ledText = self.contentsLabel.text
+			settingViewController.textColor = self.contentsLabel.textColor
+			settingViewController.backgroundColor = self.view.backgroundColor ?? .black
+		}
+	}
 
-
+	func changedSetting(text: String?, textColor: UIColor, backgroundColor: UIColor) {
+		if let text = text {
+			self.contentsLabel.text = text
+		}
+		self.contentsLabel.textColor = textColor
+		self.view.backgroundColor = backgroundColor
+	}
 }
 
